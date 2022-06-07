521cabe04fa32d5fc0ca9256e180d3b82b2d14e2
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-07 14:17:07 -0700

    Alpha version release commit

--- src/main/java/tech/nully/PluginInstaller/InstallCommand.java
+++ src/main/java/tech/nully/PluginInstaller/InstallCommand.java
@@ -1,31 +1,43 @@
 package tech.nully.PluginInstaller;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.libs.org.ibex.nestedvm.util.Seekable;
+import org.bukkit.command.ConsoleCommandSender;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Locale;
 
 public class InstallCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender snder, Command cmd, String label, String[] args) {
+        // Name checker
         if (cmd.getName().equalsIgnoreCase("installpl")) {
-            Installer ins = new Installer();
-            String Install_Jar = cmd.getName();
-            if (ins.JARURLs.containsKey(Install_Jar)) {
-                try (InputStream in = URI.create(ins.JARURLs.get(Install_Jar)).toURL().openStream()) {
-                    ins.InstallPlugin(in);
-                } catch (IOException e) {
+            snder.sendMessage("Check 1 passed");
+            if (snder.isOp() || snder instanceof ConsoleCommandSender) {
+                snder.sendMessage("Check 2 passed");
+                Installer ins = new Installer();
+                if (args.length != 1) return false;
+                snder.sendMessage("Check 3 passed");
+                String Install_Jar = args[0].toLowerCase();
+
+                // Checks if the list of plugin links contains what the user wants to install
+                if (Main.getJavaURLs().containsKey(Install_Jar)) {
+                    snder.sendMessage("Check 4 passed");
+                    // Creates an input stream based on the corresponding URL from the players first argument
+                    try (InputStream in = URI.create(Main.getJavaURLs().get(Install_Jar)).toURL().openStream()) {
+                        snder.sendMessage("Check 5 passed");
+                        // Installs the plugin
+                        ins.InstallPlugin(in, Install_Jar.toLowerCase(), snder);
+                        snder.sendMessage("You have successfully installed the " + ChatColor.GREEN + Install_Jar.toUpperCase() + ChatColor.WHITE + " plugin!");
+                        return true;
+                    } catch (IOException e) {
+                    }
                 }
-
-
             }
         }
         return false;
--- src/main/java/tech/nully/PluginInstaller/Installer.java
+++ src/main/java/tech/nully/PluginInstaller/Installer.java
@@ -1,5 +1,9 @@
 package tech.nully.PluginInstaller;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -7,14 +11,24 @@ import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 
 public class Installer {
-    public HashMap<String, String> JARURLs = new HashMap<>();
+    private HashMap<String, String> JARURLs = new HashMap<>();
 
-    public static void SetupInstaller() {
+    public static HashMap<String, String> SetupInstaller() {
         Installer ins = new Installer();
-        ins.JARURLs.put("dynmap", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/dynmap-1.9.1.jar");
+        ins.JARURLs.put("dynmap", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/dynmap-1.9.1.jar");;
+        ins.JARURLs.put("factions", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/Factions.jar");
+        ins.JARURLs.put("permissionsex", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/PermissionSex.jar");
+        ins.JARURLs.put("protocollib", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/ProtocolLib.jar");
+        ins.JARURLs.put("vault", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/Vault.jar");
+        ins.JARURLs.put("mcore", "https://github.com/darverdevs/PluginInstallerRepo/raw/main/mcore.jar");
+        return ins.JARURLs;
     }
 
-    public void InstallPlugin(InputStream in) throws IOException {
-        Files.copy(in, Main.getInstance().getDataFolder().toPath(), StandardCopyOption.REPLACE_EXISTING);
+    public void InstallPlugin(InputStream in, String pluginName, CommandSender s) throws IOException {
+        File f = new File(Main.getInstance().getDataFolder().getParent() + "/" + pluginName + ".jar");
+        s.sendMessage("Check 1x passed");
+        Files.copy(in, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        System.out.println(f.toPath().toString());
+        s.sendMessage("Check 2x passed");
     }
 }
