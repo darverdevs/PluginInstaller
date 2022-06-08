f33832c16ecbb6412afe32309ebf9f8559d87036
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-07 22:16:06 -0700

    Update Updater.java

--- src/main/java/tech/nully/PluginUpdater/Updater.java
+++ src/main/java/tech/nully/PluginUpdater/Updater.java
@@ -1,5 +1,19 @@
 package tech.nully.PluginUpdater;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 public class Updater {
-    public void UpdateInstaller
+    public void UpdateInstaller() throws IOException {
+        InputStream in = URI.create("https://github.com/darverdevs/PluginInstaller/raw/main/out/artifacts/PluginInstaller_jar/PluginInstaller.jar")
+                .toURL().openStream();
+        File f = new File(Main.getInstance().getDataFolder().getParent() + "/" + "PluginInstaller" + ".jar");
+        f.delete();
+        Files.copy(in, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
 }
