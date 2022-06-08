b0f06be3b95f45fe57abc2a6a8e31011bd1f7fa4
Fangoboyo <77935781+Fangoboyo@users.noreply.github.com>
2022-06-07 17:51:18 -0700

    updated installer again

--- src/main/java/tech/nully/PluginInstaller/Installer.java
+++ src/main/java/tech/nully/PluginInstaller/Installer.java
@@ -33,6 +33,7 @@ public class Installer {
         InputStream in = URI.create("https://github.com/darverdevs/PluginInstaller/raw/main/out/artifacts/PluginInstaller_jar/PluginInstaller.jar")
                 .toURL().openStream();
         File f = new File(Main.getInstance().getDataFolder().getParent() + "/" + "PluginInstaller" + ".jar");
+        f.delete();
         Files.copy(in, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 }
